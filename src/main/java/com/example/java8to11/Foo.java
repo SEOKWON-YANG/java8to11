f4da9b94b3f71f2a09e7d937d924a5c12ea747af
DESKTOP-KRHEU3N\양석원 <namugocps@gmail.com>
2022-03-31 15:28:34 +0900

    Initial commit

---
+++ src/main/java/com/example/java8to11/Foo.java
@@ -0,0 +1,10 @@
+package com.example.java8to11;
+
+public class Foo {
+
+    public static void main(String[] args){
+        //익명 내부 클래스
+        RunSomething runSomething = () -> System.out.println("Hello");
+        runSomething.doIt();
+    }
+}
